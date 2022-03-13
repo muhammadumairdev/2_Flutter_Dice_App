737d0c777326dab8ea22c59c8fc8b784f293b4c7
Muhammad Umair <umair68094@gmail.com>
2022-03-14 04:31:02 +0500

    Flutter_Dice_App

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,23 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice1.png'),
-          )),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice1.png'),
-          )),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
         ],
       ),
     );
